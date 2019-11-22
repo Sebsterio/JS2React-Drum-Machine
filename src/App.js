b33ac220b0e9a9c6543a19c8a42db0ebf9493241
Sebsterio <sebastian.rosloniec@gmail.com>
2019-11-22 23:29:47 +0000

    fix event listeners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Panel from "./components/panel";
 import Controls from "./components/controls";
 
@@ -10,13 +10,19 @@ class App extends React.Component {
       currentBankIndex: 0
     };
     this.display = this.display.bind(this);
-    this.toggleBank = this.toggleBank.bind(this)
+    this.toggleBank = this.toggleBank.bind(this);
   }
   render() {
     return (
       <div id="drum-machine">
-        <Panel display={this.display} currentBankIndex={this.state.currentBankIndex} />
-        <Controls displayMsg={this.state.displayMsg} toggleBank={this.toggleBank}/>
+        <Panel
+          display={this.display}
+          currentBankIndex={this.state.currentBankIndex}
+        />
+        <Controls
+          displayMsg={this.state.displayMsg}
+          toggleBank={this.toggleBank}
+        />
       </div>
     );
   }
@@ -25,8 +31,8 @@ class App extends React.Component {
   }
   toggleBank() {
     const newBankIndex = this.state.currentBankIndex === 0 ? 1 : 0;
-    const newState = {...this.state, currentBankIndex: newBankIndex};
-    this.setState(newState)
+    const newState = { ...this.state, currentBankIndex: newBankIndex };
+    this.setState(newState);
   }
 }
 
--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Pad from "./pad";
+import { keys, soundBanks } from "../data";
 
 class Panel extends React.Component {
   constructor(props) {
@@ -8,164 +9,87 @@ class Panel extends React.Component {
       keyBank: []
     };
     this.triggerPad = this.triggerPad.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
   }
 
   componentDidMount() {
     this.createKeyBank(this.props.currentBankIndex);
+    document.addEventListener("keydown", this.handleKeyDown);
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.currentBankIndex !== this.props.currentBankIndex)
-      this.createKeyBank(this.props.currentBankIndex)
+      this.createKeyBank(this.props.currentBankIndex);
   }
 
   render() {
     return (
       <div id="panel">
         {this.state.keyBank.map(pad => (
-          <Pad key={pad.id} data={pad} triggerPad={this.triggerPad} />
+          <Pad
+            key={pad.id}
+            data={pad}
+            triggerPad={this.triggerPad}
+            handleTransitionEnd={this.handleTransitionEnd}
+          />
         ))}
       </div>
     );
   }
 
   playSound(symbol) {
-    const sound = document.getElementById(symbol);
+    const sound = document.getElementById("sound-"+symbol);
     if (!sound) return;
     sound.currentTime = 0;
     sound.play();
   }
 
+  handleTransitionEnd(pad) {
+    const newKeyBank = [ ...this.state.keyBank ]
+    newKeyBank.map(key => {
+      if (key.keySymbol === pad.keySymbol) {
+        return key.isActive = false;
+      }
+    })
+    this.setState({
+      keyBank: newKeyBank
+    })
+  }
+
   triggerPad(pad) {
     this.playSound(pad.keySymbol);
     this.props.display(pad.id);
+    const newKeyBank = [ ...this.state.keyBank ]
+    newKeyBank.map(key => {
+      if (key.keySymbol === pad.keySymbol) {
+        return key.isActive = true;
+      }
+    })
+    this.setState({
+      keyBank: newKeyBank
+    })
+  }
+
+  handleKeyDown(e) {
+    const currentPad = this.state.keyBank.filter(key => {
+      return key.keyCode === e.keyCode;
+    })[0];
+    this.triggerPad(currentPad)
   }
 
   createKeyBank(bankIndex) {
     this.props.display("Sound Bank #" + bankIndex);
     const newBank = [];
     for (let i = 0; i < keys.length; i++) {
-      newBank.push({ ...keys[i], ...soundBanks[bankIndex][i] });
+      newBank.push({
+        ...keys[i],
+        ...soundBanks[bankIndex][i],
+        isActive: false
+      });
     }
     this.setState({ keyBank: newBank });
   }
 }
-
-const keys = [
-  {
-    keyCode: 81,
-    keySymbol: "Q"
-  },
-  {
-    keyCode: 87,
-    keySymbol: "W"
-  },
-  {
-    keyCode: 69,
-    keySymbol: "E"
-  },
-  {
-    keyCode: 65,
-    keySymbol: "A"
-  },
-  {
-    keyCode: 83,
-    keySymbol: "S"
-  },
-  {
-    keyCode: 68,
-    keySymbol: "D"
-  },
-  {
-    keyCode: 90,
-    keySymbol: "Z"
-  },
-  {
-    keyCode: 88,
-    keySymbol: "X"
-  },
-  {
-    keyCode: 67,
-    keySymbol: "C"
-  }
-];
-const soundBanks = [
-  [
-    {
-      id: "Heater-1",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-    },
-    {
-      id: "Heater-2",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-    },
-    {
-      id: "Heater-3",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-    },
-    {
-      id: "Heater-4",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-    },
-    {
-      id: "Clap",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-    },
-    {
-      id: "Open-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-    },
-    {
-      id: "Kick-n'-Hat",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-    },
-    {
-      id: "Kick",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-    },
-    {
-      id: "Closed-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-    }
-  ],
-  [
-    {
-      id: "Chord-1",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
-    },
-    {
-      id: "Chord-2",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
-    },
-    {
-      id: "Chord-3",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
-    },
-    {
-      id: "Shaker",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
-    },
-    {
-      id: "Open-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
-    },
-    {
-      id: "Closed-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
-    },
-    {
-      id: "Punchy-Kick",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
-    },
-    {
-      id: "Side-Stick",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-    },
-    {
-      id: "Snare",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-    }
-  ]
-];
 
 export default Panel;
